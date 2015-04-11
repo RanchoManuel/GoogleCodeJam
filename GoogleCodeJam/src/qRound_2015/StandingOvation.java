c9fc4d1855e7884f27e04430e7ffced8d86fba34
Juan Manuel <jmanuel816@gmail.com>
2015-04-10 20:17:03 -0500

    Proyecto revisado en 2015

---
+++ GoogleCodeJam/src/qRound_2015/StandingOvation.java
@@ -0,0 +1,25 @@
+package qRound_2015;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class StandingOvation
+{
+	private static PrintWriter pr;
+	private static boolean test=true;
+	
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br=new BufferedReader(new FileReader(new File("")));
+		if(test) pr=new PrintWriter(new File(""));
+		else pr=new PrintWriter(System.out);
+		//----------------------------------------------------------------
+		
+		//----------------------------------------------------------------
+		br.close();pr.close();
+	}
+}
