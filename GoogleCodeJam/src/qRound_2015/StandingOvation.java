0cc9de6efeadaff356f86ec7759076afca84fc8d
Juan Manuel <jmanuel816@gmail.com>
2015-04-10 20:59:36 -0500

    Primer problema del qRound 2015

--- GoogleCodeJam/src/qRound_2015/StandingOvation.java
+++ GoogleCodeJam/src/qRound_2015/StandingOvation.java
@@ -6,20 +6,43 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
 public class StandingOvation
 {
-	private static PrintWriter pr;
-	private static boolean test=true;
+	private static String archivo="A-large";
+	private static boolean test=false;
 	
 	public static void main(String[] args) throws IOException
 	{
-		BufferedReader br=new BufferedReader(new FileReader(new File("")));
-		if(test) pr=new PrintWriter(new File(""));
-		else pr=new PrintWriter(System.out);
-		//----------------------------------------------------------------
+		PrintWriter pr;
+		if(test) pr=new PrintWriter(System.out);
+		else pr=new PrintWriter(new File("./data/qRound_2015/StandingOvation/"+archivo+".out"));
+		BufferedReader br=new BufferedReader(new FileReader(new File("./data/qRound_2015/StandingOvation/"+archivo+".in")));
+		//--------------------------------------------------------------------------------------
+		int casos=Integer.parseInt(br.readLine()), sMax, rta, genteDePie;
 		
-		//----------------------------------------------------------------
+		char[] publico;
+		String aux[];
+		
+		for(int c=1; c<=casos; c++)
+		{
+			rta=genteDePie=0;
+			aux=br.readLine().split(" ");
+			sMax=Integer.parseInt(aux[0]);
+			publico=aux[1].toCharArray();
+			
+			for(int i=0; i<=sMax; i++)
+			{
+				if(genteDePie<i)
+				{
+					int losQueHacenFalta=i-genteDePie;
+					rta+=losQueHacenFalta;
+					genteDePie+=losQueHacenFalta;
+				}
+				genteDePie+=publico[i]-'0';
+			}
+			pr.printf("Case #%d: %d\n",c, rta);
+		}
+		//--------------------------------------------------------------------------------------
 		br.close();pr.close();
 	}
 }
