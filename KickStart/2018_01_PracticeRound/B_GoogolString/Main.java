0c48199bb9a9291ff9953feee5fafcd074febfa0
Juan Manuel <jmgonzalezgarzon@gmail.com>
2019-01-08 10:36:34 -0500

    Problema B, solo la carpeta

---
+++ KickStart/2018_01_PracticeRound/B_GoogolString/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main
+{
+	public static void main(String[] args) throws Exception
+	{
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			int casos = Integer.parseInt(br.readLine());
+			for (int i = 0; i < casos; i++)
+			{
+				//Separar informacion pertinente
+
+				String rta = solucionarCaso();
+
+				//---------------------------------------------
+
+				String solucion = "Case #"+(i+1)+": "+rta;
+				System.out.println(solucion);
+			}
+		}
+	}
+
+	private static String solucionarCaso() 
+	{
+		String rta="";
+		return rta;
+	}
+}
