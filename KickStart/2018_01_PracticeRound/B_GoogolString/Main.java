474a776897e49f877a6014da320933b8b9c9f5d9
Juan Manuel <jmgonzalezgarzon@gmail.com>
2019-01-09 11:00:54 -0500

    Guardar avances es la solucion del problema B de la ronda de practicas del 2018

--- KickStart/2018_01_PracticeRound/B_GoogolString/Main.java
+++ KickStart/2018_01_PracticeRound/B_GoogolString/Main.java
@@ -8,11 +8,12 @@ public class Main
 	{
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 			int casos = Integer.parseInt(br.readLine());
+			construirSecuencia();
 			for (int i = 0; i < casos; i++)
 			{
 				//Separar informacion pertinente
-
-				String rta = solucionarCaso();
+				long desiredNumber = Long.parseLong(br.readLine());
+				String rta = solucionarCaso(desiredNumber);
 
 				//---------------------------------------------
 
@@ -22,9 +23,24 @@ public class Main
 		}
 	}
 
-	private static String solucionarCaso() 
+	private static void construirSecuencia() {
+		StringBuilder sb0 = new StringBuilder();
+		StringBuilder sb1 = new StringBuilder();
+		for (int i=0; i<10; i++) {
+			String secondPart = sb0.reverse().toString();
+			secondPart = secondPart.replaceAll("0", "_");
+			secondPart = secondPart.replace("1", "0");
+			secondPart = secondPart.replace("_", "1");
+
+			sb1 = sb1.append("0").append(secondPart);
+			System.out.println(i + ": " + sb1);
+			sb0 = sb1;
+		}
+	}
+
+	private static String solucionarCaso(long d)
 	{
-		String rta="";
+		String rta="" + d;
 		return rta;
 	}
 }
